c6e0dc3a75d4b801c3910e3d8dc1a33714e7d780
Sophia Sklaviadis <ssklaviadis@gmail.com>
2021-07-18 17:50:05 -0400

    1hot-depcrf

--- depcrf.py
+++ depcrf.py
@@ -6,6 +6,8 @@ import torch_struct.data
 import torchtext.data as data
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+import torch.optim as optim
+
 
 def batch_num(nums):
     lengths = torch.tensor([len(n) for n in nums]).long()
@@ -15,27 +17,24 @@ def batch_num(nums):
         out[b, :len(n)] = torch.tensor(n)
     return out, lengths
 HEAD = data.RawField(preprocessing= lambda x: [int(i) for i in x],
-                     postprocessing=batch_num)
-HEAD.is_target = True
-
+                     postprocessing=batch_num, 
+                     is_target = True)
 WORD = data.Field(pad_token=None)
-WORD.is_target = False
-
 train = torch_struct.data.ConllXDataset("samIam.conllu", (('word', WORD), ('head', HEAD)),
                      ) #filter_pred=lambda x: 5 < len(x.word) < 40
-
+val = torch_struct.data.ConllXDataset("samIam.conllu", (('word', WORD), ('head', HEAD)),
+                     ) #  filter_pred=lambda x: 5 < len(x.word[0]) < 40
 WORD.build_vocab(train)
 train_iter = data.BucketIterator(train, batch_size=2, device='cpu', shuffle=False)
+val_iter = data.BucketIterator(val, batch_size=2, device="cpu", shuffle=False)
 
 V = len(WORD.vocab.itos)
-
 
 class Model(nn.Module):
     def __init__(self, hidden):
         super().__init__()
 
         self.embedding = nn.Embedding.from_pretrained(F.one_hot(torch.arange(V)).type(torch.FloatTensor), freeze=True) #one hot 
-
         self.linear = nn.Linear(V, V)
         self.bilinear = nn.Linear(V, V)
 
@@ -56,41 +55,82 @@ class Model(nn.Module):
         return final
 
 model = Model(V)
+opt = optim.SGD(model.parameters(), lr=0.01)
 
 def show_deps(tree):
     plt.imshow(tree.detach())
 
-def trn(train_iter, model):
-
-    for i, ex in enumerate(train_iter):
-        print(i)
-        # print(ex.word.shape) # sq x b
-        # print(ex.head[0].shape) # b x sq
-        # print('lens', ex.head[1])
-        
+def validate(val_iter):
+    incorrect_edges = 0
+    total_edges = 0
+    model.eval()
+    for i, ex in enumerate(val_iter):
         words = ex.word.transpose(0,1)
-        #print(words.shape, mapper.shape)
         label, lengths = ex.head
-        #batch, _ = label.shape
-        
-        final = model(words)
-        print(final)
+
+        final = model(words) #.cuda()
+
         dist = DependencyCRF(final, lengths=lengths)
-        # dist.multiroot=False
-        # show_deps(dist.argmax[0])
-        # plt.show()
+        argmax = dist.argmax
+        gold = dist.struct.to_parts(label, lengths=lengths).type_as(argmax)
+        incorrect_edges += (argmax[:, :].cpu() - gold[:, :].cpu()).abs().sum() / 2.0
+        total_edges += gold.sum()
 
-        labels = dist.struct.to_parts(label, lengths=lengths).type_as(final)
-        #print('labels', labels.shape)
+        gold1 = DependencyCRF(gold, lengths=lengths)
 
-        gold = DependencyCRF(labels, lengths=lengths)
-        show_deps(gold.argmax[0])
-        plt.show()
+    print(total_edges, incorrect_edges)   
+    model.train()
+    return gold1
 
-        log_prob = dist.log_prob(labels)
-        print(log_prob.shape)
+def trn(train_iter, model):
+    for epoch in range(20):
+        model.train()
+        losses = []
 
-        loss = log_prob.sum()
-        (-loss).backward()
+        for i, ex in enumerate(train_iter):
+            print(i)
+            # print(ex.word.shape) # sq x b
+            # print(ex.head[0].shape) # b x sq
+            # print('lens', ex.head[1])
+            
+            words = ex.word.transpose(0,1)
+            label, lengths = ex.head
+            #batch, _ = label.shape
+            
+            final = model(words)
+            #print(model.embedding.weight)
+            #print(final)
+            dist = DependencyCRF(final, lengths=lengths)
+            # dist.multiroot=False
+
+            # show_deps(dist.argmax[0])
+            # plt.show()
+
+            labels = dist.struct.to_parts(label, lengths=lengths).type_as(final)
+            #print('labels', labels.shape)
+
+            gold = DependencyCRF(labels, lengths=lengths)
+            # show_deps(gold.argmax[0])
+            # plt.show()
+
+            log_prob = dist.log_prob(labels)
+            #print(log_prob.shape)
+
+            loss = log_prob.sum()
+            (-loss).backward()
+            losses.append(loss.detach())
+            opt.step()
+
+
+        if epoch % 10 == 1:            
+            print(i, -torch.tensor(losses).mean(), words.shape)
+            losses = []
+            show_deps(dist.argmax[0])
+            plt.show()
+            
+        if epoch % 10 == 1:
+            gold = validate(val_iter)        
+            show_deps(gold.argmax[0])
+            plt.show()
 
 trn(train_iter, model)
