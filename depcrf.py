6f8a5fa4fccf265ed7b23c35f47cb2ae5e27e0ef
Sophia Sklaviadis <ssklaviadis@gmail.com>
2021-07-18 21:04:24 -0400

    deptree0

--- depcrf.py
+++ depcrf.py
@@ -8,7 +8,6 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import torch.optim as optim
 
-
 def batch_num(nums):
     lengths = torch.tensor([len(n) for n in nums]).long()
     n = lengths.max()
@@ -20,9 +19,9 @@ HEAD = data.RawField(preprocessing= lambda x: [int(i) for i in x],
                      postprocessing=batch_num, 
                      is_target = True)
 WORD = data.Field(pad_token=None)
-train = torch_struct.data.ConllXDataset("samIam.conllu", (('word', WORD), ('head', HEAD)),
+train = torch_struct.data.ConllXDataset("wsj.train0.conllx", (('word', WORD), ('head', HEAD)),
                      ) #filter_pred=lambda x: 5 < len(x.word) < 40
-val = torch_struct.data.ConllXDataset("samIam.conllu", (('word', WORD), ('head', HEAD)),
+val = torch_struct.data.ConllXDataset("wsj.train0.conllx", (('word', WORD), ('head', HEAD)),
                      ) #  filter_pred=lambda x: 5 < len(x.word[0]) < 40
 WORD.build_vocab(train)
 train_iter = data.BucketIterator(train, batch_size=2, device='cpu', shuffle=False)
@@ -34,14 +33,14 @@ class Model(nn.Module):
     def __init__(self, hidden):
         super().__init__()
 
-        self.embedding = nn.Embedding.from_pretrained(F.one_hot(torch.arange(V)).type(torch.FloatTensor), freeze=True) #one hot 
+        self.embedding = nn.Embedding.from_pretrained(torch.eye(V).type(torch.FloatTensor), freeze=True) #one hot 
+        # F.one_hot(torch.arange(V))
         self.linear = nn.Linear(V, V)
         self.bilinear = nn.Linear(V, V)
 
         self.root = nn.Parameter(torch.rand(V))
         
     def forward(self, words):
-        #out = self.dropout(self.base_model(words)[0])
         out = self.embedding(words) # (b x N ) -> (b x N x V)
         final2 = self.linear(out) # (b x N x V) (V x V) -> (b x N x V)
         final = torch.einsum("bnh,hg,bmg->bnm", out, self.bilinear.weight, final2) # (N x V) (V x V) (V x N) -> (N, N)
@@ -55,7 +54,7 @@ class Model(nn.Module):
         return final
 
 model = Model(V)
-opt = optim.SGD(model.parameters(), lr=0.01)
+opt = optim.SGD(model.parameters(), lr=1e-4)
 
 def show_deps(tree):
     plt.imshow(tree.detach())
@@ -83,12 +82,12 @@ def validate(val_iter):
     return gold1
 
 def trn(train_iter, model):
-    for epoch in range(20):
+    for epoch in range(100):
         model.train()
         losses = []
 
         for i, ex in enumerate(train_iter):
-            print(i)
+            #print(i)
             # print(ex.word.shape) # sq x b
             # print(ex.head[0].shape) # b x sq
             # print('lens', ex.head[1])
@@ -103,15 +102,8 @@ def trn(train_iter, model):
             dist = DependencyCRF(final, lengths=lengths)
             # dist.multiroot=False
 
-            # show_deps(dist.argmax[0])
-            # plt.show()
-
             labels = dist.struct.to_parts(label, lengths=lengths).type_as(final)
             #print('labels', labels.shape)
-
-            gold = DependencyCRF(labels, lengths=lengths)
-            # show_deps(gold.argmax[0])
-            # plt.show()
 
             log_prob = dist.log_prob(labels)
             #print(log_prob.shape)
@@ -125,12 +117,12 @@ def trn(train_iter, model):
         if epoch % 10 == 1:            
             print(i, -torch.tensor(losses).mean(), words.shape)
             losses = []
-            show_deps(dist.argmax[0])
-            plt.show()
+            # show_deps(dist.argmax[0])
+            # plt.show()
             
         if epoch % 10 == 1:
             gold = validate(val_iter)        
-            show_deps(gold.argmax[0])
-            plt.show()
+            # show_deps(gold.argmax[0])
+            # plt.show()
 
 trn(train_iter, model)
